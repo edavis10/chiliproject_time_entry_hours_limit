083a59fd16f47c14adb4a135e8cac0c7078cac9a
Eric Davis <edavis@littlestreamsoftware.com>
2011-07-15 11:25:42 -0700

    Generated plugin

---
+++ init.rb
@@ -0,0 +1,10 @@
+require 'redmine'
+
+Redmine::Plugin.register :chiliproject_time_entry_hours_limit do
+  name 'Chiliproject Time Entry Hours Limit plugin'
+  author 'Author name'
+  description 'This is a plugin for ChiliProject'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
